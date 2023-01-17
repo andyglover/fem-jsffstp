811f7e6d653202f457ed49aa04cc82a8e0e943e7
Andy Glover <andrew.l.glover@gmail.com>
2023-01-16 22:43:15 -0800

    create my object

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello, world!");
+
+const andy = {
+  name: "Andy",
+  home: "Seattle",
+  pet: "Karma",
+  vehicle: "Chevy",
+  studying: ["html", "css", "javascript"],
+  wantToLearn: ["react", "sql", "node.js"],
+};
