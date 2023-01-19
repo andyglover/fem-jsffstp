8a37f106e0bb08f75d665f5aa43a0b34c9ce0128
Andy Glover <andrew.l.glover@gmail.com>
2023-01-18 18:28:32 -0800

    do first conditionals exercise

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ const andy = {
   wantToLearn: ["react", "sql", "node.js"],
 };
 
+//CONDITIONALS EXERCISE
 //write a conditional that logs a message saying whether your first name or last name is longer
+function firstOrLastLonger(firstName, lastName) {
+  if (firstName.length > lastName.length) {
+    console.log("first is longer!");
+  } else if (firstName.length < lastName.length) {
+    console.log("last is longer!");
+  } else if (firstName.length == lastName.length) {
+    console.log("equal length!");
+  } else {
+    console.log("umm...");
+  }
+}
+
 //write a function isEmpty(array) that returns whether a given array is empty or not
 // Is an empty array truthy or falsy? write a conditional to find outer
