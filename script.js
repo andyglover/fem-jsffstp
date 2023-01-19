bb90b23fcff4888f1bcf689ee44a690769344c46
Andy Glover <andrew.l.glover@gmail.com>
2023-01-18 18:23:39 -0800

    add stylesheet. add conditionals exercise in comments

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ const andy = {
   studying: ["html", "css", "javascript"],
   wantToLearn: ["react", "sql", "node.js"],
 };
+
+//write a conditional that logs a message saying whether your first name or last name is longer
+//write a function isEmpty(array) that returns whether a given array is empty or not
+// Is an empty array truthy or falsy? write a conditional to find outer
